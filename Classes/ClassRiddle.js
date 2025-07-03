5805e551c47659fc38c6699e56ede23e7c90a238
Moti <shalom7950@gmail.com>
2025-07-03 11:51:40 +0300

    Start refactor from old repo

---
+++ Classes/ClassRiddle.js
@@ -0,0 +1,26 @@
+import readline from 'readline-sync';
+import {AllRiddles} from '../Riddles/AllRiddlesImport.js';
+
+export class Riddles
+{
+    idCount = Object.keys(AllRiddles).length;
+    // WhitchGame = 0;
+    constructor (WhitchGame)
+    {
+        for (const questin of AllRiddles[WhitchGame])
+        { 
+            let userAnswer = '';
+            while (userAnswer !== questin.correctAnswer)
+            { 
+                console.log(`question: ${questin.taskDescription}`);
+                userAnswer = readline.question(`Write answer: `)
+
+                if (userAnswer !== questin.correctAnswer)
+                {
+                    console.log("Incorrect 😢. Try agein");  
+                }
+            }
+        console.log("Correct 🙂");
+        }   
+    }
+}
