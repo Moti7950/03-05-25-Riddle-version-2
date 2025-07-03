c929980c91edbc512ebdec12075f580a0d34346a
Moti7950 <shalom7950@gmail.com>
2025-07-03 20:47:30 +0300

    Update Create func

--- Utility/DAL.js
+++ Utility/DAL.js
@@ -1,18 +1,44 @@
-import { readFile } from "node:fs/promises"
+import { readFile,writeFile } from "node:fs/promises"
+import readlineSync from "readline-sync";
 
-async function Create()
-{
+
+//Utility function
+function MaxId(path) {
+    let counter = 1;
+    // arrayOfObjects = JSON.parse(Read(path))
+    path.forEach(item => {
+        if ("ID" in item) {
+            counter += 1;
+        }
+    });
+
+    return counter;
+}
+
+async function Create(DBPath, name, TimeStatistics) {
+    const templitJson =
+    {
+        "ID": MaxId(DBPath),
+        "name": name,
+        "TimeStatistics": TimeStatistics
+    };
+
+    console.log("Cehck print from func Create: ", templitJson);
+    try {
+        await writeFile("../DB/TestingFile.txt", JSON.stringify(templitJson));
+        console.log("Write successful!");
+    } catch (err) {
+        console.error("Error writing file:", err);
+    }
 
 }
 
 async function Read(path) {
-    try
-    {
+    try {
         const asyncRead = await readFile(path, "utf8")
-        return asyncRead;
+        return JSON.parse(asyncRead);
     }
-    catch (err)
-    {   
+    catch (err) {
         return err;
     }
 }
@@ -26,5 +52,12 @@ async function Delete() {
 }
 
 const res = await Read("../DB/TestingFile.txt");
-console.log(res);
+console.log(res, "\n", "Type:", typeof res);
+// const testReadLine = readlineSync.question("Enter name: " )
+// console.log("Hi", testReadLine);
+console.log(MaxId(res));
+Create(res, "Moti", "5")
+
+
+
 
